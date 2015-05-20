ee746d307518d18c7de4b229c6d007c638fcb94c
lexandro <lexandro2000@gmail.com>
2015-05-20 20:47:28 +0200

    Adding error codes

---
+++ app/src/main/java/com/lexandro/integration/model/ErrorCode.java
@@ -0,0 +1,15 @@
+package com.lexandro.integration.model;
+
+public enum ErrorCode {
+
+    USER_ALREADY_EXISTS,
+    USER_NOT_FOUND,
+    ACCOUNT_NOT_FOUND,
+    MAX_USERS_REACHED,
+    UNAUTHORIZED,
+    OPERATION_CANCELED,
+    CONFIGURATION_ERROR,
+    INVALID_RESPONSE,
+    UNKNOWN_ERROR,
+    PENDING
+}
