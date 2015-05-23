18a4f697108705ac01ceaadff27e836a360b7fac
lexandro <lexandro2000@gmail.com>
2015-05-23 14:40:59 +0200

    Fake commit to trigger hook

--- app/src/main/java/com/lexandro/integration/service/OAuthSignedEventService.java
+++ app/src/main/java/com/lexandro/integration/service/OAuthSignedEventService.java
@@ -30,7 +30,7 @@ public class OAuthSignedEventService implements EventService {
         log.debug("OAuthSignedEventService get with {}", url);
         try {
             String signedUrl = oAuthConsumer.sign(url);
-            log.info("Signed URL {}", signedUrl);
+            log.debug("Signed URL {}", signedUrl);
             String result = httpService.get(signedUrl);
             log.debug("Get result {}", result);
             T eventObject = xmlService.toObject(result, expectedEventType);
